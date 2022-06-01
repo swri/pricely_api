d306a113df3804996adc4d7b0362b1241a3f0996
swri <aripsuwari@gmail.com>
2022-06-01 23:42:28 +0700

    change for redirect ssl

--- src/server.js
+++ src/server.js
@@ -8,9 +8,38 @@ const fs = require("fs");
 require("dotenv").config();
 
 const init = async () => {
-  const server = hapi.server({
+  const redirect = hapi.server({
     host: process.env.HOST,
     port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  redirect.route([
+    {
+      method: "*",
+      path: "/{any*}",
+      handler: (_, h) => {
+        return h.redirect("https://34.124.235.106");
+      },
+    },
+    {
+      method: "*",
+      path: "/{redirect}",
+      handler: (req, h) => {
+        return h.redirect(`https://34.124.235.106/${req.params.redirect}`);
+      },
+    },
+  ]);
+
+  await redirect.start();
+
+  const server = hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORTSSL,
     tls: {
       key: fs.readFileSync(path.resolve("src/utils/private.key")),
       cert: fs.readFileSync(path.resolve("src/utils/certificate.crt")),
